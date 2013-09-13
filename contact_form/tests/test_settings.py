e63bca20e4980c9ebd7685bde6617bdfbbc1f752
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-09-13 08:33:43 +0200

    Added manange.py, south and test settings, CHANGELOG and updated requirements

--- contact_form/tests/test_settings.py
+++ contact_form/tests/test_settings.py
@@ -2,44 +2,37 @@
 import os
 
 DEBUG = True
+
 SITE_ID = 1
 
-CONTACT_FORM_RECIPIENTS = (
-    ('Test', 'test@example.com'),
-)
+APP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
 
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
-
-CURRENT_DIR = os.path.dirname(__file__)
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
     }
 }
 
 ROOT_URLCONF = 'contact_form.tests.urls'
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(CURRENT_DIR, '../../static/')
-
+STATIC_ROOT = os.path.join(APP_ROOT, '../app_static')
+MEDIA_ROOT = os.path.join(APP_ROOT, '../app_media')
 STATICFILES_DIRS = (
-    os.path.join(CURRENT_DIR, 'test_static'),
+    os.path.join(APP_ROOT, 'static'),
 )
 
 TEMPLATE_DIRS = (
-    os.path.join(CURRENT_DIR, './templates'),
+    os.path.join(APP_ROOT, 'tests/test_app/templates'),
 )
 
 COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join(
-    CURRENT_DIR, 'coverage')
-
+    os.path.join(APP_ROOT, 'tests/coverage'))
 COVERAGE_MODULE_EXCLUDES = [
-    'tests$', 'test_app$', 'settings$', 'urls$', 'locale$',
+    'tests$', 'settings$', 'urls$', 'locale$',
     'migrations', 'fixtures', 'admin$', 'django_extensions',
 ]
 
@@ -53,6 +46,7 @@ EXTERNAL_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'django.contrib.sites',
+    'django_nose',
 ]
 
 INTERNAL_APPS = [
@@ -60,5 +54,20 @@ INTERNAL_APPS = [
 ]
 
 INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
-
 COVERAGE_MODULE_EXCLUDES += EXTERNAL_APPS
+
+SECRET_KEY = 'foobar'
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_translation.middleware.MultilingualGenericsMiddleware'
+)
+
+CONTACT_FORM_RECIPIENTS = (
+    ('Test Contact', 'test@example.com'),
+)
